20412e4b34199cc83ca53a9100639c736f4284cd
Huang <xin.huang.ext@siemens.com>
2022-10-26 13:46:29 +0800

    clearRepo AND larger font

--- src/components/AddPwd.vue
+++ src/components/AddPwd.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 
 function addPwd(formList: any): boolean {
   const formData: any = {};
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
-  
+function clearRepo() {
+  const confirmed = confirm('确定清空数据库吗？此操作不可逆！');
+  if (confirmed) {
+    window.indexedDB.open('AccountManager').onsuccess = function(openE: any): any {
+      const db = openE.target.result;
+      const objectStore = db.transaction('accounts', 'readwrite').objectStore('accounts');
+      objectStore.openCursor().onsuccess = (cursorE: any) => {
+        let cursor = cursorE.target.result;
+        if (cursor) {
+          objectStore.delete(cursor.key).onsuccess = function() {
+            cursor.continue();
+          }
+        } else {
+          alert('数据库已清空！')
+        }
+      }
+    }
+  }
+}
 </script>
 
 <template>
@@ -10,6 +28,7 @@
     <button @click="$emit('changeRoute', '/modify')">修改账户</button>
     <button @click="$emit('changeRoute', '/view')">查看所有账户</button>
     <button @click="$emit('changeRoute', '/import')">批量导入账户</button>
+    <button @click="clearRepo()">清空数据库</button>
   </div>
 </template>
 
